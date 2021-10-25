072ff8db2a9235b15c91f2dca288d20da2bbdd92
aayushi221 <aayushipandey221@gmail.com>
2021-10-25 16:04:47 +0530

    Restructured code

---
+++ routes/passport.js
@@ -0,0 +1,115 @@
+const router = require("express").Router();
+// Bring in the User Registration function
+const {
+  userAuth,
+  userLogin,
+  checkRole,
+  userRegister,
+  serializeUser
+} = require("../utils/Auth");
+
+// Users Registeration Route
+/**
+ * @swagger
+ * /passport/register-user:
+ *    post:
+ *      description: Use to register user
+ *      responses:
+ *        '400':
+ *          description: Username is already taken.
+ *        '201':
+ *          description: Hurry! now you are successfully registred. Please nor login.
+ *        '500':
+ *          description: Unable to create your account.
+ *    produces:
+ *       - application/json
+ *    parameters:
+ *       - name: username
+ *         in: formData
+ *         required: true
+ *         type: string
+ *       - name: email
+ *         in: formData
+ *         required: true
+ *         type: string
+ *       - name: password
+ *         in: formData
+ *         required: true
+ *         type: string
+ */
+
+router.post("/register-user", async (req, res) => {
+  await userRegister(req.body, "user", res);
+});
+
+// Admin Registration Route
+router.post("/register-admin", async (req, res) => {
+  await userRegister(req.body, "admin", res);
+});
+
+// Super Admin Registration Route
+router.post("/register-super-admin", async (req, res) => {
+  await userRegister(req.body, "superadmin", res);
+});
+
+// Users Login Route
+router.post("/login-user", async (req, res) => {
+  await userLogin(req.body, "user", res);
+});
+
+// Admin Login Route
+router.post("/login-admin", async (req, res) => {
+  await userLogin(req.body, "admin", res);
+});
+
+// Super Admin Login Route
+router.post("/login-super-admin", async (req, res) => {
+  await userLogin(req.body, "superadmin", res);
+});
+
+// Profile Route
+router.get("/profile", userAuth, async (req, res) => {
+  return res.json(serializeUser(req.user));
+});
+
+// Users Protected Route
+router.get(
+  "/user-protectd",
+  userAuth,
+  checkRole(["user"]),
+  async (req, res) => {
+    return res.json("Hello User");
+  }
+);
+
+// Admin Protected Route
+router.get(
+  "/admin-protectd",
+  userAuth,
+  checkRole(["admin"]),
+  async (req, res) => {
+    return res.json("Hello Admin");
+  }
+);
+
+// Super Admin Protected Route
+router.get(
+  "/super-admin-protectd",
+  userAuth,
+  checkRole(["superadmin"]),
+  async (req, res) => {
+    return res.json("Hello Super Admin");
+  }
+);
+
+// Super Admin Protected Route
+router.get(
+  "/super-admin-and-admin-protectd",
+  userAuth,
+  checkRole(["superadmin", "admin"]),
+  async (req, res) => {
+    return res.json("Super admin and Admin");
+  }
+);
+
+module.exports = router;
